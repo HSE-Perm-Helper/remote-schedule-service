fcf8b5ff359229a0e4ed213342c44ad9a9b75272
Denis Malinin <melowetty@mail.ru>
2024-11-07 02:45:04 +0500

    add calendar token service impl and added tests

--- src/main/kotlin/ru/melowetty/remotescheduleservice/service/CalendarTokenService.kt
+++ src/main/kotlin/ru/melowetty/remotescheduleservice/service/CalendarTokenService.kt
@@ -1,6 +1,9 @@
 package ru.melowetty.remotescheduleservice.service
 
+import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.stereotype.Service
+import ru.melowetty.remotescheduleservice.entity.CalendarTokenEntity
+import ru.melowetty.remotescheduleservice.exception.CalendarTokenNotFoundException
 import ru.melowetty.remotescheduleservice.repository.CalendarTokenRepository
 
 @Service
@@ -8,14 +11,31 @@ class CalendarTokenService(
     private val calendarTokenRepository: CalendarTokenRepository,
 ) {
     fun verifyToken(telegramId: Long, token: String): Boolean {
-        return false
+        val realToken = getToken(telegramId)
+        return realToken == token
     }
 
-    fun createToken(telegramId: Long, token: String): String {
-        return ""
+    fun createOrUpdateToken(telegramId: Long): String {
+        val generatedToken = generateToken()
+        val currentTokenEntity = calendarTokenRepository.findById(telegramId).orElseGet {
+            CalendarTokenEntity(telegramId, generatedToken)
+        }
+
+        calendarTokenRepository.save(currentTokenEntity.copy(
+            token = generatedToken
+        ))
+
+        return generatedToken
     }
 
-    fun checkExistsToken(telegramId: Long, token: String): Boolean {
-        return false
+    fun getToken(telegramId: Long): String {
+        val tokenEntity = calendarTokenRepository.findById(telegramId)
+            .orElseThrow {  throw CalendarTokenNotFoundException("Ключ для этого пользователя не найден!") }
+
+        return tokenEntity.token
+    }
+
+    fun generateToken(): String {
+        return RandomStringUtils.randomAlphanumeric(64)
     }
 }
